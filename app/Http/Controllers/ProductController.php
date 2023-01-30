02c84a8ec8f31b5593d4ce5f82d34f0d9c204722
Henry percy <henryppercy@gmail.com>
2023-01-30 22:55:27 +0000

    Created category model

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,8 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products', ['products' => $products]);
+        $categories = Category::all();
+        return view('products', ['categories' => $categories ,'products' => $products]);
     }
 
     /**
