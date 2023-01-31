7b48c41ffdfff1963bfd45d1a88d06777a976cc5
Henry percy <henryppercy@gmail.com>
2023-01-31 11:24:47 +0000

    Made the products view responsive for mobile

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,9 +6,9 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <title>Rework Digital Laravel Task</title>
     </head>
-    <body class="bg-slate-500 grid justify-center">
+    <body class="bg-slate-500 flex justify-center gap-9 m-6 lg:flex-row flex-col">
         <div class="w-full max-w-xs grid mb-10">
-            <form action="/products" method="POST" class="mt-9 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form action="/products" method="POST" class="mt-9 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 h-min">
                 @csrf
                 <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Product name: </label>
                 <input type="text" name="name" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -33,19 +33,23 @@
                 <button type="submit" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Add product</button>
             </form>
         </div>
-        <h1 class="text-white font-sans text-5xl font-medium grow">Products</h1>
-        <div class="flex flex-row flex-wrap">
-            @foreach ($products as $product)
-            <div class="block max-w-sm p-6 border rounded-lg shadow hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700 m-4">
-                <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">{{ $product->name }}</h2>
-                <p class="text-xl font-bold text-white">£{{ $product->price }}</p>
-                <p class="font-normal text-gray-400">Quantity: {{ $product->quantity }}</p>
-                <p class="font-normal text-gray-400">{{ $product->description }}</p>  
-                @foreach ($product->categories as $category)
-                    <li class="font-normal text-gray-400">{{ $category->name }}</li>
-                @endforeach  
-            </div>    
-            @endforeach
+        <div>
+            <h1 class="text-white font-sans text-5xl font-medium p-6">Products</h1>
+            <div class="flex flex-row flex-wrap">
+                @foreach ($products as $product)
+                <div class="block max-w-sm p-6 border rounded-lg shadow hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700 m-4">
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">{{ $product->name }}</h2>
+                    <p class="text-xl font-bold text-white">£{{ $product->price }}</p>
+                    <p class="font-normal text-gray-400 italic">Quantity: {{ $product->quantity }}</p>
+                    <p class="font-normal text-white mt-4">{{ $product->description }}</p> 
+                    <div class="mt-3">
+                        @foreach ($product->categories as $category)
+                            <p class="font-normal text-gray-500 italic inline">{{ $category->name }}</p>
+                        @endforeach  
+                    </div>
+                </div>    
+                @endforeach
+            </div>
         </div>
     </body>
 </html>
