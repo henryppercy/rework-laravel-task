56147c1c09c6795aa04d6cd3a393994a7e08e2d0
Henry percy <henryppercy@gmail.com>
2023-02-06 09:49:29 +0000

    Removed whitespace

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -24,10 +24,10 @@
 
                 <label for="category" class="block text-gray-700 text-sm font-bold mb-2">Category:</label>
                 @foreach ($categories as $category)
-                <div class="flex items-center mb-4">
-                    <input name="category[]" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2" value="{{ $category->id }}">
-                    <label for="category" class="ml-2 text-sm font-medium text-gray-900">{{ $category->name }}</label>
-                </div>
+                    <div class="flex items-center mb-4">
+                        <input name="category[]" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2" value="{{ $category->id }}">
+                        <label for="category" class="ml-2 text-sm font-medium text-gray-900">{{ $category->name }}</label>
+                    </div>
                 @endforeach
 
                 <button type="submit" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Add product</button>
@@ -37,17 +37,17 @@
             <h1 class="text-white font-sans text-5xl font-medium p-6">Products</h1>
             <div class="flex flex-row flex-wrap">
                 @foreach ($products as $product)
-                <div class="block max-w-sm p-6 border rounded-lg shadow hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700 m-4">
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">{{ $product->name }}</h2>
-                    <p class="text-xl font-bold text-white">£{{ $product->price }}</p>
-                    <p class="font-normal text-gray-400 italic">Quantity: {{ $product->quantity }}</p>
-                    <p class="font-normal text-white mt-4">{{ $product->description }}</p> 
-                    <div class="mt-3">
-                        @foreach ($product->categories as $category)
-                            <p class="font-normal text-gray-500 italic inline">{{ $category->name }}</p>
-                        @endforeach  
-                    </div>
-                </div>    
+                    <div class="block max-w-sm p-6 border rounded-lg shadow hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700 m-4">
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-white">{{ $product->name }}</h2>
+                        <p class="text-xl font-bold text-white">£{{ $product->price }}</p>
+                        <p class="font-normal text-gray-400 italic">Quantity: {{ $product->quantity }}</p>
+                        <p class="font-normal text-white mt-4">{{ $product->description }}</p> 
+                        <div class="mt-3">
+                            @foreach ($product->categories as $category)
+                                <p class="font-normal text-gray-500 italic inline">{{ $category->name }}</p>
+                            @endforeach  
+                        </div>
+                    </div>    
                 @endforeach
             </div>
         </div>
